e4b37331d7cffdc447e667b826de60110877a760
EssienAbasiama <essienabasiama11@gmail.com>
2022-08-19 21:42:10 -0700

    Alert

--- src/component/Alert/Confirmation.jsx
+++ src/component/Alert/Confirmation.jsx
@@ -3,6 +3,14 @@ import './Confirmation.css';
 import Party from "./../../images/party.png";
 
 function Confirmation() {
+   const copyLink = () => {
+     /* Get the text field */
+     var transferLink = "https://nestlypaywaitlist-api.herokuapp.com";
+     /* Copy the text inside the text field */
+     navigator.clipboard.writeText(transferLink);
+     /* Alert the copied text */
+     alert("Copied the text: " + transferLink);
+   };
   return (
     <div className="alert animate-zoom">
       {/* <div className='cancel-container'>
@@ -13,8 +21,8 @@ function Confirmation() {
       <span>Create and send invoices, manage your finance,</span>
       <span>track sales, and get paid faster.</span>
       <div className="buttonContainer">
-        <button className="joinUs">Join Our Community</button>
-        <button className="share">Share to Your Friends</button>
+        <button href='' className="joinUs">Join Our Community</button>
+        <button onClick={copyLink} href='' className="share">Share to Your Friends</button>
       </div>
     </div>
   );
